20eaaf772ca0010bb9f039a8a546150b9ab23c9c
Md Mohsin Sarkar <90359439+mdmohsinsarkarariyan@users.noreply.github.com>
2026-03-30 12:47:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+  // 👉 Typing animation
+  const texts = ["আমরা কাজ করি আপনার জন্যে", " আমাদের কাজে থাকে নিশ্চয়তা", ];
+  let count = 0;
+  let index = 0;
+  let currentText = '';
+  let letter = '';
+
+  function type() {
+    if (count === texts.length) {
+      count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.typing-text').textContent = letter;
+
+    if (letter.length === currentText.length) {
+      count++;
+      index = 0;
+      setTimeout(type, 1500); // বিরতি
+    } else {
+      setTimeout(type, 150);
+    }
+  }
+  type();
+
+
+
+    
